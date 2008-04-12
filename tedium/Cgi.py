8ffdce170eae5c50cfc9255ea6a7bd199aa0c7de
james <james@94f099bb-ef87-469a-bf14-b0303b48b6ab>
2008-04-12 19:31:25 +0000

    include-replies -> include-replies-to (and friends) jinja output option
    git-svn-id: svn+ssh+userv://atreus.tartarus.org/sja/tedium@64 94f099bb-ef87-469a-bf14-b0303b48b6ab

--- tedium/Cgi.py
+++ tedium/Cgi.py
@@ -23,16 +23,40 @@ Tedium's CGI driver.
 
 import tedium
 
-import re, os, sys, cgi, cgitb, urllib
+import re, os, os.path, sys, cgi, cgitb, urllib
+from jinja import Template, Context, FileSystemLoader
+from jinja.filters import stringfilter
+from jinja.lib import stdlib
+
+linkifier = re.compile('[a-zA-Z]+://[a-zA-Z0-9\.-]*[^ )?#]*(\?[^ )#]*)?(#[^ )]*)?')
+user_linkifier = re.compile('@([A-Za-z0-9_]+)')
+
+def htmlify(text):
+    # link anything using common URI
+    text = re.sub(linkifier, '<a target="_new" href="\g<0>">\g<0></a>', text)
+    # link @username
+    text = re.sub(user_linkifier, '<a target="_new" href="http://twitter.com/\g<1>">\g<0></a>', text)
+    return text
+
+def safe_attribute(text):
+    # make it safe to put into an attribute...
+    text = text.replace('&', '&amp;')
+    text = text.replace('"', '&quot;')
+    text = text.replace("'", '&apos;')
+    text = text.replace('<', '&lt;')
+    text = text.replace('>', '&gt;')
+    return text
+
+stdlib.register_filter('htmlify', stringfilter(htmlify))
+stdlib.register_filter('safe_attribute', stringfilter(safe_attribute))
 
 class Driver:
     """Tedium's CGI driver; construct with a Tedium object, then call do_get()."""
-    def __init__(self, tedium, is_test=False):
+    def __init__(self, _tedium, is_test=False):
         """Initialise driver with a given Tedium object."""
-        self.tedium = tedium
+        self.tedium = _tedium
         self.is_test = is_test
-        self.linkifier = re.compile('[a-zA-Z]+://[a-zA-Z0-9\.-]*[^ )?#]*(\?[^ )#]*)?(#[^ )]*)?')
-        self.user_linkifier = re.compile('@([A-Za-z0-9_]+)')
+        self.templates_dir = os.path.join(os.path.dirname(tedium.__file__), '../templates')
 
     def _auth(self):
         if self.is_test:
@@ -50,38 +74,26 @@ class Driver:
     def _address(self):
         return (u"<address>tedium v%s copyright <a href='http://tartarus.org/james/'>James Aylett</a>.</address>" % (tedium.VERSION,)).encode('utf-8')
 
-    def _stylesheet(self):
+    def _ponder_stylesheet(self):
         cssfile = None
         for file in os.listdir('.'):
             if file[-4:]=='.css':
                 cssfile = file
                 break
+        return cssfile
+
+    def _stylesheet(self):
+        cssfile = self._ponder_stylesheet()
         if cssfile!=None:
             return "<link type='text/css' rel='stylesheet' href='%s' />" % cssfile
         else:
             return ""
 
-    def _htmlify(self, text):
-        # link anything using common URI
-        text = re.sub(self.linkifier, '<a target="_new" href="\g<0>">\g<0></a>', text)
-        # link @username
-        text = re.sub(self.user_linkifier, '<a target="_new" href="http://twitter.com/\g<1>">\g<0></a>', text)
-        return text
-
-    def _safe_attribute(self, text):
-        # make it safe to put into an attribute...
-        text = text.replace('&', '&amp;')
-        text = text.replace('"', '&quot;')
-        text = text.replace("'", '&apos;')
-        text = text.replace('<', '&lt;')
-        text = text.replace('>', '&gt;')
-        return text
-
     def _headers(self):
         print "Content-Type: text/html; charset=utf-8\r\n"
 
     def _html_head(self, title):
-        print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "">'
+        print '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
         print (u"<html lang='en' xml:lang='en' xmlns='http://www.w3.org/1999/xhtml'><head><title>%s</title>" % title).encode('utf-8')
         print self._stylesheet();
         print "</head>"
@@ -105,7 +117,7 @@ class Driver:
         self._auth()
         cgitb.enable()
 
-        if form.getfirst('set-author-include-replies')!=None:
+        if form.getfirst('set-author-include-replies-to')!=None:
             # author-<aid> = <include|>
             for ak in form.keys():
                 if ak.startswith('author-'):
@@ -115,9 +127,9 @@ class Driver:
                         include = 1
                     else:
                         include = 0
-                    self.tedium.update_author_include_replies(aid, include)
+                    self.tedium.update_author_include_replies_to(aid, include)
                     
-            authors = form.getlist('author-include-replies')
+            authors = form.getlist('author-include-replies-to')
             self.tedium.save_changes()
         elif form.getfirst('update-status')!=None:
             new_status = form.getfirst('status')
@@ -158,50 +170,79 @@ class Driver:
         if not self.is_test and reset_digest!=None:
             self.tedium.set_conf('last_digest', reset_digest)
 
+        if form.has_key('out') and form.getfirst('out')=='jinja':
+            jinja = True
+        else:
+            jinja = False
+
         self._headers()
-        self._html_head('Tweets for %s' % self.tedium.username)
-        print "<body>"
+        if not jinja:
+            self._html_head('Tweets for %s' % self.tedium.username)
+            print "<body>"
         last_viewed = self.tedium.get_conf('last_viewed')
         last_digest = self.tedium.get_conf('last_digest')
         tweets = self.tedium.tweets_to_view(20, replies) # min to display
 
         my_status = self.tedium.get_conf('current_status')
-        if my_status!=None:
-            print (u"<form id='status-form' method='post'><label for='status'>Current status</label>: <input type='text' name='status' value='%s' id='status' width='140' size='140' /><input type='submit' name='update-status' value='&gt;&gt; Update!' /></form>" % (self._safe_attribute(my_status))).encode('utf-8')
+        if not jinja and my_status!=None:
+            print (u"<form id='status-form' method='post'><label for='status'>Current status</label>: <input type='text' name='status' value='%s' id='status' width='140' size='140' /><input type='submit' name='update-status' value='&gt;&gt; Update!' /></form>" % (safe_attribute(my_status))).encode('utf-8')
 
         if len(tweets)>0:
-            print "<ol>"
+            if not jinja:
+                print "<ol>"
             for tweet in tweets:
                 author = tweet['author']
                 rowstyle=""
                 if tweet['published']>last_viewed:
+                    tweet['_is_new'] = True
                     rowstyle+=" new"
                 if tweet['published']>last_digest:
+                    tweet['_is_notindigest'] = True
                     rowstyle+=" notindigest"
                 if author['protected']:
+                    tweet['_is_protected'] = True
                     rowstyle+=" protected"
-                print (u"<li class='%s'><span class='time'>%s</span> " % (rowstyle, tweet['date'])).encode('utf-8')
-                if author['nick']==self.tedium.username:
-                    print (u"<span class='author'><a href='http://twitter.com/%s'>%s</a></span>" % (author['nick'], author['fn'])).encode('utf-8')
+                if not jinja:
+                    print (u"<li class='%s'><span class='time'>%s</span> " % (rowstyle, tweet['date'])).encode('utf-8')
+                if author['is_me']:
+                    if not jinja:
+                        print (u"<span class='author'><a href='http://twitter.com/%s'>%s</a></span>" % (author['nick'], author['fn'])).encode('utf-8')
                 else:
-                    if author['include_replies']==1:
+                    if author['include_replies_to']==1:
                         a_r_flip = 'hide'
                         a_r_flip_text = 'hide replies'
                     else:
                         a_r_flip = 'show'
                         a_r_flip_text = 'show replies'
-                    print (u"<span class='author'><form method='post'><input type='hidden' name='author-%i' value='%s' /><a href='http://twitter.com/%s'>%s</a> [<input type='submit' name='set-author-include-replies' value='%s' />]</form></span>" % (author['id'], a_r_flip, author['nick'], author['fn'], a_r_flip_text)).encode('utf-8')
-                print (u"<span class='tweet'>%s</span></li>" % self._htmlify(tweet['tweet'])).encode('utf-8')
-            print "</ol>"
-        
-        if replies=='digest':
-            repliesinfo = 'Showing some replies. <a href="?replies=all&amp;last_viewed=%s&amp;last_digest=%s">Show all replies</a>.' % (last_viewed, last_digest)
-        else:
-            repliesinfo = 'Showing all replies. <a href="?replies=digest&amp;last_viewed=%s&amp;last_digest=%s">Show fewer replies</a>.' % (last_viewed, last_digest)
+                    if not jinja:
+                        print (u"<span class='author'><form method='post'><input type='hidden' name='author-%i' value='%s' /><a href='http://twitter.com/%s'>%s</a> [<input type='submit' name='set-author-include-replies-to' value='%s' />]</form></span>" % (author['id'], a_r_flip, author['nick'], author['fn'], a_r_flip_text)).encode('utf-8')
+                if not jinja:
+                    print (u"<span class='tweet'>%s</span></li>" % htmlify(tweet['tweet'])).encode('utf-8')
+            if not jinja:
+                print "</ol>"
 
-        print (u"<p><a href='http://twitter.com/'>Twitter</a> interface for <a href='http://twitter.com/%s'>%s</a>. %s</p>" % (self.tedium.username, self.tedium.username, repliesinfo)).encode('utf-8')
-        print self._address()
-        print "</body></html>"
+        if not jinja:
+            if replies=='digest':
+                repliesinfo = 'Showing some replies. <a href="?replies=all&amp;last_viewed=%s&amp;last_digest=%s">Show all replies</a>.' % (last_viewed, last_digest)
+            else:
+                repliesinfo = 'Showing all replies. <a href="?replies=digest&amp;last_viewed=%s&amp;last_digest=%s">Show fewer replies</a>.' % (last_viewed, last_digest)
+
+            print (u"<p><a href='http://twitter.com/'>Twitter</a> interface for <a href='http://twitter.com/%s'>%s</a>. %s</p>" % (self.tedium.username, self.tedium.username, repliesinfo)).encode('utf-8')
+            print self._address()
+            print "</body></html>"
+
+        if jinja:
+            tmpl = Template('main', FileSystemLoader(self.templates_dir))
+            c = Context({
+                'my_status': my_status,
+                'tedium': self.tedium,
+                'tweets': tweets,
+                'last_viewed': last_viewed,
+                'last_digest': last_digest,
+                'replies': replies,
+                'cssfile': self._ponder_stylesheet()
+                })
+            print tmpl.render(c).encode('utf-8')
         if not self.is_test and len(tweets)>0:
             max_published = max(map(lambda x: x['published'], tweets))
             self.tedium.set_conf('last_viewed', max_published)
